fcd009ccca7c5b7b1b37b95ae7b874c273e89731
Liza Tarasova <tarasova.liza.vic@gmail.com>
2024-10-03 20:54:43 +0100

    Add string formatting to improve output readability

---
+++ investment_calculator/go.mod
@@ -0,0 +1,3 @@
+module github.com/Elyzavetka/investment-calculator
+
+go 1.23.2
---
+++ investment_calculator/investment_calculator.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const inflationRate = 2.5
+	var investmentAmount float64
+	var years float64
+	expectedReturnRate := 5.5
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected Return Rates: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years) 
+	futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years)
+
+	formattedFV := fmt.Sprintf("Future Value: %.0f\n", futureValue)
+	formattedRFV := fmt.Sprintf("Future Value (adjasted for inflation): %.0f\n", futureRealValue)
+
+	// Output information
+	// fmt.Println("Future Value:", futureValue)
+	// fmt.Printf("Future Value: %.0f\n", futureValue)
+	// fmt.Printf("Future Value (adjasted for inflation): %.0f\n",futureRealValue)
+	fmt.Print(formattedFV, formattedRFV)
+}
